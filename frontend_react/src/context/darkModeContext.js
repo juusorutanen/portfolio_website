6c0d7bd530b621a31dac268d131758f2e5cdb94f
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-02-15 12:01:21 +0200

    update darkmode as default

--- frontend_react/src/context/darkModeContext.js
+++ frontend_react/src/context/darkModeContext.js
@@ -7,7 +7,7 @@ export const DarkModeContext = createContext ()
 
 export const DarkModeContextProvider = ({ children }) => {
     const [darkMode, setDarkMode] = useState(
-      JSON.parse(localStorage.getItem("darkMode")) || false
+      JSON.parse(localStorage.getItem("darkMode")) || true
     );
   
     const toggle = () => {
