977ce16bb3654a6729c97c11add4ba552a90c9f8
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-02-22 22:58:34 +0200

    update header text

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -14,9 +14,9 @@ const Header = () => {
       <div className='app__header-text-container'>
         <h1 className="app__header-text">👋 Hi! I'm Juuso.</h1>
         <h2 className="app__header-text-smaller">I do web development.</h2>
-        <p className="app__header-text-paragraph">I'm a recent Business Information Technology graduate and my passion is web development. Currently I'm open to any vacant trainee/junior roles in the field of web development. 
+        <p className="app__header-text-paragraph">I'm a recent Business Information Technology graduate and my passion is web development. Currently I'm seeking for any vacant trainee/junior roles in the field of web development. 
         At the moment I'm learning React.js and enhancing my knowledge in JavaScript and CSS. I live in Helsinki, Finland.</p>
-        <p className="app__header-text-paragraph">Please note that this website is currently <span>very unfinished.</span></p>
+        <p className="app__header-text-paragraph">Please note that this website is currently <span> unfinished</span> and will be continously updated.</p>
         
           <a href="#projects">
           <motion.button
@@ -34,7 +34,7 @@ const Header = () => {
       <div className='app__person'>
         <div className='app__person-container'>
           <div className='app__person-circle'></div>
-          <img className='app__person-img' src={images.juusoprofilephoto} alt ="Juuso Rutanen Profile photo"/>
+          <img className='app__person-img' src={images.juusoprofilephoto} alt ="Juuso Rutanen Profile"/>
         </div>
       </div>
     </div>
