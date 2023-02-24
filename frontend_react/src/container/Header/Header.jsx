be138fc8a6438df08fa5fd3b36759a0b7f25f1cb
Juuso Rutanen <juuso.rutanen@gmail.com>
2023-02-24 17:59:35 +0200

    create animations

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -6,17 +6,39 @@ import { images } from '../../constants';
 
 import { AppWrap } from '../../wrapper';
 
+let boxVariants = {};
+const isMobile = window.innerWidth < 768; 
+if (!isMobile) {
+  boxVariants = {
+    whileInViewLeft: {
+      x: [-350, 0],
+      opacity: [0, 1],
+      transition: {duration:1.5}
+    },
+    whileInViewRight: {
+      x: [250, 0],
+      opacity: [0, 1],
+      transition: {duration:1.5}
+    },
+  };
+}
+
 
 const Header = () => {
   return (
+    
+    
     <div className='app__flex app__header-container'>
-      <div className='app__header-text-container'>
+      <motion.div 
+      variants={boxVariants}
+      whileInView= "whileInViewLeft"
+      className='app__header-text-container'>
+      
         <h1 className="app__header-text">👋 Hi! I'm Juuso.</h1>
         <h2 className="app__header-text-smaller">I do <span>web development.</span></h2>
         <p className="app__header-text-paragraph">I'm a recent Business Information Technology graduate and my passion is web development. Currently I'm seeking for any vacant trainee/junior roles in the field of web development. 
         At the moment I'm learning React.js and enhancing my knowledge in JavaScript and CSS. I live in Helsinki, Finland.</p>
         <p className="app__header-text-paragraph">Please note that this website is currently <span> unfinished</span> and will be continously updated.</p>
-        
           <a href="#projects">
           <motion.button
           whileHover={{scale:1.1}}
@@ -24,19 +46,24 @@ const Header = () => {
           type="button" className="app__header-cta-text"
           >View my projects</motion.button>
           </a>
-        <div className='app__social-mobile'>
+          <div className='app__social-mobile'>
             <div> <a href="https://github.com/juusorutanen" target="_blank" rel="noopener noreferrer"><BsGithub  /> </a></div>
             <div> <a href="https://www.instagram.com/juusoruta" target="_blank" rel="noopener noreferrer"><BsInstagram /></a></div>
             <div> <a href="https://www.linkedin.com/in/juusorutanen/" target="_blank" rel="noopener noreferrer"><BsLinkedin /></a> </div>
         </div>
-      </div>
-      <div className='app__person'>
+          </motion.div>
+          
+      <motion.div 
+      variants={boxVariants}
+      whileInView="whileInViewRight"
+      className='app__person'>
         <div className='app__person-container'>
           <div className='app__person-circle'></div>
           <img className='app__person-img' src={images.juusoprofilephoto} alt ="Juuso Rutanen Profile"/>
         </div>
-      </div>
+      </motion.div>
     </div>
+    
     
   )
 }
