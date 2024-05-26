a5ff0a6f3f2b4e13ca41b9780ad2349c25059f63
Juuso Rutanen <juuso.rutanen@gmail.com>
2024-05-26 20:46:57 +0300

    style: add gap between icons + add aria-labels

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
-import { BsGithub, BsInstagram, BsLinkedin} from 'react-icons/bs';
+import React from "react";
+import { BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const SocialMedia = () => {
-    return (
-        <div className='app__social'>
-            <div>
-                <a href="https://github.com/juusorutanen" target="_blank" rel="noopener noreferrer">
-                <BsGithub />
-                </a>
-            </div>
-            <div>
-                <a href="https://www.instagram.com/juusoruta" target="_blank" rel="noopener noreferrer">
-                <BsInstagram />
-                </a>
-            </div>
-            <div>
-            <a href="https://www.linkedin.com/in/juusorutanen/" target="_blank" rel="noopener noreferrer">
-                <BsLinkedin />
-                </a>
-            </div>
-            <span className='app__vertical-line-left'></span>
-        </div>
-    )
-}
+  return (
+    <div className="app__social">
+      <div>
+        <a
+          href="https://github.com/juusorutanen"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub profile"
+        >
+          <BsGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/juusoruta"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram profile"
+        >
+          <BsInstagram />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/juusorutanen/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Linkedin profile"
+        >
+          <BsLinkedin />
+        </a>
+      </div>
+      <span className="app__vertical-line-left"></span>
+    </div>
+  );
+};
 
 export default SocialMedia;
